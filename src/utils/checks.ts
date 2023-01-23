1cb4abce393aed4f5ef20ae8a8877e2059d39704
Bozai Cosmin <cosminbozai@outlook.com>
2023-01-23 19:35:04 +0000

    edited the check for functions

--- src/utils/checks.ts
+++ src/utils/checks.ts
@@ -8,7 +8,7 @@ export function getCustomError(value: string): string {
       return "Factorial operators can't be the first element";
     case /^\^/.test(value):
       return "You need a value before the exponent sign";
-    case /abs|cos|sin|tan|sqrt/.test(value):
+    case /abs\s|abs$|cos\s|cos$|sin\s|sin$|tan\s|tan$|sqrt\s|sqrt$/.test(value):
       return "Functions require arguments. For example: abs(-2)";
     default:
       return "Could not evaluate expression";
